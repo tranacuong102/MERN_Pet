9651503d4377967b83bffcdbe6e85a74b8603f09
Your Name <you@example.com>
2022-07-26 04:10:59 +0700

    Fix Netlify

--- server/src/index.js
+++ server/src/index.js
@@ -25,12 +25,12 @@ db.connectDB()
 route(app)
 
 // Build production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 // PORT
 const PORT = process.env.PORT || 5000
